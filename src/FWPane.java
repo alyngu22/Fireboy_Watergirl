0e0dcbb74e254c46c172606831585eb23bd96457
Alyssa Nguyen <alyngu22@bergen.org>
2019-05-29 12:08:43 -0400

    commit

--- src/FWPane.java
+++ src/FWPane.java
@@ -1,16 +1,21 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class FWPane extends Application {
     @Override
 
-    public void start (Stage primaryStage) {
-        BorderPane pane = new BorderPane();
+    public void start (Stage firstStage) {
+        BorderPane bp = new BorderPane();
+        Pane pane = new Pane();
 
-        Scene scene = new Scene(pane, 1500,950);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+
+
+
+        Scene scene = new Scene(bp, 1500,950);
+        firstStage.setScene(scene);
+        firstStage.show();
     }
 }
