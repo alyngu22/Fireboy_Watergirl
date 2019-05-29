ba44a831f01c980bc9cdc5ef576fa99d1cc22cfa
Alyssa Nguyen <alyngu22@bergen.org>
2019-05-29 12:03:28 -0400

    commit

--- src/FWPane.java
+++ src/FWPane.java
@@ -1,7 +1,6 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class FWPane extends Application {
