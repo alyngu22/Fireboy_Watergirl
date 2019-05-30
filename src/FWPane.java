847c58774cc23041bd48cb669afbbd6141d7c7ab
isabelting1 <isatin22@bergen.org>
2019-05-30 11:44:52 -0400

    hi

--- src/FWPane.java
+++ src/FWPane.java
@@ -21,40 +21,4 @@ public class FWPane extends Application {
         firstStage.setScene(scene);
         firstStage.show();
     }
-
-    public class Watergirl extends Pane {
-        public Watergirl{
-            Circle head = new Circle(20);
-            head.setOnKeyPressed(e->{
-                if (e.getCode() == KeyCode.LEFT) {
-
-                }
-            });
-        }
-        public void front(Circle fHead) {
-            Arc smile = new Arc();
-            smile.setRadiusX(10);
-            smile.setRadiusY(5);
-            smile.centerYProperty().bind(fHead.centerXProperty().add(4));
-            smile.centerYProperty().bind(fHead.centerYProperty().subtract(4));
-            smile.setStartAngle(40);
-            smile.setType(ArcType.OPEN);
-            smile.setRotate(115);
-            fHead.setFill(Color.BLUE);
-            fHead.setStroke(Color.BLUE);
-        }
-        public void left() {
-            Circle fHead = new Circle(20);
-            Arc smile = new Arc();
-            smile.setRadiusX(10);
-            smile.setRadiusY(5);
-            smile.centerYProperty().bind(fHead.centerXProperty().add(4));
-            smile.centerYProperty().bind(fHead.centerYProperty().subtract(4));
-            smile.setStartAngle(40);
-            smile.setType(ArcType.OPEN);
-            smile.setRotate(115);
-            fHead.setFill(Color.BLUE);
-            fHead.setStroke(Color.BLUE);
-        }
-    }
 }
