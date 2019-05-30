59d67ea5c7e3ec73ed464bf008843eeafc5e2316
Alyssa Nguyen <alyngu22@bergen.org>
2019-05-30 12:04:53 -0400

    commit

--- src/FWPane.java
+++ src/FWPane.java
@@ -1,31 +1,37 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Arc;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class FWPane extends Application {
     @Override
 
     public void start (Stage firstStage) {
-        BorderPane bp = new BorderPane();
-        bp.setStyle("-fx-background-color: rgb(1,50,32)");
 
         Pane pane = new Pane();
-        pane.setStyle("-fx-background-color: rgba(256,256,256,256)");
+        pane.setStyle("-fx-background-color: rgb(0,0,0)");
+
+        //Fireboy
+
+
+        Circle head = new Circle(100,825,40);
+        head.setFill(Color.RED);
+        head.setStroke(Color.RED);
 
 
 
 
-        Scene scene = new Scene(bp, 1500,950);
+
+
+        pane.getChildren().addAll(head);
+
+        Scene scene = new Scene(pane, 1500,950);
         firstStage.setScene(scene);
         firstStage.show();
     }
 }
 
-class Fireboy extends Pane {
-    Arc mouth = new Arc(300,375,140,100,180,180);
 
-}
+
