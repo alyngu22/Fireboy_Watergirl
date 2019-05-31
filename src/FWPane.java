c3927cffbb307396fc830175a6bc69eab6476d3f
Alyssa Nguyen <alyngu22@bergen.org>
2019-05-31 11:33:57 -0400

    commit

--- src/FWPane.java
+++ src/FWPane.java
@@ -1,8 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class FWPane extends Application {
@@ -14,19 +14,27 @@ public class FWPane extends Application {
         pane.setStyle("-fx-background-color: rgb(0,0,0)");
 
         //Fireboy
+        ImageView fireboy = new ImageView("fireboy.png");
+        fireboy.setFitWidth(85);
+        fireboy.setFitHeight(85);
+        fireboy.setX(60);
+        fireboy.setY(800);
 
+        pane.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.UP){
+                fireboy.setY(fireboy.getY()-15);
+            }
 
-        Circle head = new Circle(100,825,40);
-        head.setFill(Color.RED);
-        head.setStroke(Color.RED);
+            if (e.getCode() == KeyCode.LEFT) {
+                fireboy.setX(fireboy.getX()+15);
+            }
 
+            if (e.getCode() == KeyCode.RIGHT) {
+                fireboy.setX(fireboy.getX()-15);
+            }
+        });
 
-
-
-
-
-        pane.getChildren().addAll(head);
-
+        pane.getChildren().add(fireboy);
         Scene scene = new Scene(pane, 1500,950);
         firstStage.setScene(scene);
         firstStage.show();
