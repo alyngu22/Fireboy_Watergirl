d815e6f29c7d84f51cf34a7a1f4df02215794f10
Alyssa Nguyen <alyngu22@bergen.org>
2019-06-02 19:42:40 -0400

    commit

--- src/FWPane.java
+++ src/FWPane.java
@@ -1,9 +1,11 @@
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class FWPane extends Application {
     @Override
@@ -20,26 +22,63 @@ public class FWPane extends Application {
         fireboy.setX(60);
         fireboy.setY(800);
 
+        //jumpUp
+
+        TranslateTransition jumpUp = new TranslateTransition();
+        jumpUp.setDuration(Duration.millis(750));
+        jumpUp.setNode(fireboy);
+        jumpUp.setByY(-75);
+        jumpUp.setCycleCount(1);
+
+        //jumpDown
+        TranslateTransition jumpDown = new TranslateTransition();
+        jumpDown.setDuration(Duration.millis(750));
+        jumpDown.setNode(fireboy);
+        jumpDown.setByY(75);
+        jumpDown.setCycleCount(1);
+
+        //moveRight
+        TranslateTransition moveRight = new TranslateTransition();
+        moveRight.setDuration(Duration.millis(1750));
+        moveRight.setNode(fireboy);
+        moveRight.setByX(100);
+        moveRight.setCycleCount(1);
+
+
+        //moveLeft
+        TranslateTransition moveLeft = new TranslateTransition();
+        moveLeft.setDuration(Duration.millis(1750));
+        moveLeft.setNode(fireboy);
+        moveLeft.setByX(-100);
+        moveLeft.setCycleCount(1);
+
         pane.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.UP){
-                fireboy.setY(fireboy.getY()-15);
-            }
+            if (e.getCode() == KeyCode.UP) {
+                    jumpUp.play();
+                }
 
-            if (e.getCode() == KeyCode.LEFT) {
-                fireboy.setX(fireboy.getX()+15);
-            }
+                if (e.getCode() == KeyCode.LEFT) {
+                    moveLeft.play();
+                }
 
-            if (e.getCode() == KeyCode.RIGHT) {
-                fireboy.setX(fireboy.getX()-15);
+                if (e.getCode() == KeyCode.RIGHT) {
+                    moveRight.play();
+                }
+
+        });
+
+        pane.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.UP) {
+                jumpDown.play();
             }
         });
 
-        pane.getChildren().add(fireboy);
+
+        pane.getChildren().addAll(fireboy);
         Scene scene = new Scene(pane, 1500,950);
         firstStage.setScene(scene);
         firstStage.show();
-    }
+        pane.requestFocus();
+
 }
-
-
-
+}
