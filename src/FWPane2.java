f82fb6c81e73231bdba48318ac5cb0f68a6fcf0d
isabelting1 <isatin22@bergen.org>
2019-06-05 12:03:49 -0400

    hi

--- src/FWPane2.java
+++ src/FWPane2.java
@@ -1,70 +1,66 @@
+
+import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class FWPane2 extends Application {
     @Override
-
-    public void start(Stage firstStage) {
+    public void start (Stage firstStage) {
 
         Pane pane = new Pane();
         pane.setStyle("-fx-background-color: rgb(0,0,0)");
+
+        int groundY = 450;
+
 
         //Fireboy
         ImageView fireboy = new ImageView("fireboy.png");
         fireboy.setFitWidth(85);
         fireboy.setFitHeight(85);
         fireboy.setX(60);
-        fireboy.setY(800);
+        fireboy.setY(groundY);
 
-        //Watergirl
-        ImageView watergirl = new ImageView("fireboy.png");
-        watergirl.setFitWidth(85);
-        watergirl.setFitHeight(85);
-        watergirl.setX(60);
-        watergirl.setY(800);
-
-        //jumpUp
 
         TranslateTransition jumpUp = new TranslateTransition();
-        jumpUp.setDuration(Duration.millis(750));
-        jumpUp.setNode(watergirl);
-        jumpUp.setByY(-75);
+        jumpUp.setDuration(Duration.millis(500));
+        jumpUp.setNode(fireboy);
+        jumpUp.setByY(-150);
         jumpUp.setCycleCount(1);
 
-        //jumpDown
         TranslateTransition jumpDown = new TranslateTransition();
-        jumpDown.setDuration(Duration.millis(750));
-        jumpDown.setNode(watergirl);
-        jumpDown.setByY(75);
+        jumpDown.setDuration(Duration.millis(500));
+        jumpDown.setNode(fireboy);
+        jumpDown.setByY(150);
         jumpDown.setCycleCount(1);
 
-        //moveRight
+        SequentialTransition jump = new SequentialTransition(jumpUp,jumpDown);
+
         TranslateTransition moveRight = new TranslateTransition();
-        moveRight.setDuration(Duration.millis(1750));
-        moveRight.setNode(watergirl);
-        moveRight.setByX(100);
+        moveRight.setDuration(Duration.millis(550));
+        moveRight.setNode(fireboy);
+        moveRight.setByX(150);
         moveRight.setCycleCount(1);
 
-
-        //moveLeft
         TranslateTransition moveLeft = new TranslateTransition();
-        moveLeft.setDuration(Duration.millis(1750));
-        moveLeft.setNode(watergirl);
-        moveLeft.setByX(-100);
+        moveLeft.setDuration(Duration.millis(550));
+        moveLeft.setNode(fireboy);
+        moveLeft.setByX(-150);
         moveLeft.setCycleCount(1);
 
-        pane.setOnKeyTyped(e -> {
-            if (e.getCode() == KeyCode.A) {
-                jumpUp.play();
-                jumpDown.play();
-            }
 
+        pane.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.UP) {
+                jump.play();
+            }
             if (e.getCode() == KeyCode.LEFT) {
                 moveLeft.play();
             }
@@ -72,15 +68,46 @@ public class FWPane2 extends Application {
             if (e.getCode() == KeyCode.RIGHT) {
                 moveRight.play();
             }
-
         });
 
-        pane.getChildren().addAll(fireboy);
-        pane.getChildren().addAll(watergirl);
-        Scene scene = new Scene(pane, 1500, 950);
+        pane.setOnKeyReleased( ev -> {
+            if (ev.getCode() == KeyCode.LEFT) {
+                moveLeft.pause();
+            }
+            if (ev.getCode() == KeyCode.RIGHT) {
+                moveRight.pause();
+            }
+        });
+
+
+        pane.getChildren().addAll(fireboy, new firePane(150, 490));
+
+        Scene scene = new Scene(pane, 500,550);
         firstStage.setScene(scene);
         firstStage.show();
         pane.requestFocus();
 
+    }
+
+    class hedgePane extends Pane{
+        public hedgePane (int x,int y) {
+            ImageView hedge = new ImageView("hedgeImage.jpg");
+            hedge.setFitHeight(125);
+            hedge.setFitWidth(150);
+            hedge.setY(y);
+            hedge.setX(x);
+            getChildren().add(hedge);
+
+        }
+
+    }
+
+    class firePane extends Pane {
+        public firePane (int x, int y) {
+            Arc fire = new Arc(x,y,75,25,180,180);
+            fire.setType(ArcType.ROUND);
+            fire.setFill(Color.RED);
+            getChildren().add(fire);
+        }
     }
 }
