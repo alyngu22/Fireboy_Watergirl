6642fa7aa8912b19cf3095d178a099e51064d94e
isabelting1 <isatin22@bergen.org>
2019-06-12 22:56:54 -0400

    will add gems

--- src/FWPaneFinal.java
+++ src/FWPaneFinal.java
@@ -10,11 +10,15 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 import javafx.scene.text.*;
 public class FWPaneFinal extends Application {
+    private int wGem;
+    private int fGem;
     public void start(Stage firstStage) {
+        wGem = 0;
+        fGem = 0;
         Pane pane = new Pane();
         pane.setStyle("-fx-background-color: rgb(0,0,0)");
 
-        int groundY = 550;
+        int groundY = 825;
 
         ImageView fireboy = new ImageView("fireboy.png");
         fireboy.setFitWidth(85);
@@ -30,9 +34,32 @@ public class FWPaneFinal extends Application {
         water.setType(ArcType.CHORD);
         water.setFill(Color.BLUE);
 
-        Rectangle stair = new Rectangle(550, 430, 200, 300);
+        Rectangle stair = new Rectangle(550, 735, 200, 300);
         stair.setStroke(Color.WHITE);
 
+        ImageView bGem = new ImageView("bluegem.png");
+        bGem.setFitWidth(30);
+        bGem.setFitHeight(30);
+        bGem.setX(570);
+        bGem.setY(stair.getY()-30);
+
+        ImageView bGem1 = new ImageView("bluegem.png");
+        bGem1.setFitWidth(30);
+        bGem1.setFitHeight(30);
+        bGem1.setX(30);
+        bGem1.setY(groundY-30);
+
+        ImageView rGem = new ImageView("rGem.png");
+        rGem.setFitHeight(35);
+        rGem.setFitWidth(30);
+        rGem.setX(340);
+        rGem.setY(735);
+
+        ImageView rGem1 = new ImageView("rGem.png");
+        rGem1.setFitHeight(35);
+        rGem1.setFitWidth(30);
+        rGem1.setX(810);
+        rGem1.setY(745);
         //jump movement Fireboy
         KeyValue sKV = new KeyValue(fireboy.yProperty(), fireboy.getY()-125, Interpolator.EASE_BOTH);
         KeyFrame sKF = new KeyFrame(Duration.millis(450), sKV);
@@ -139,6 +166,22 @@ public class FWPaneFinal extends Application {
                     ns.stop();
                     ns1.stop();
                 }
+                if(fireboy.contains(rGem.getX(), rGem.getY())){
+                    pane.getChildren().remove(rGem);
+                    fGem += 1;
+                }
+                if(fireboy.contains(rGem1.getX(), rGem1.getY())){
+                    pane.getChildren().remove(rGem1);
+                    fGem +=1;
+                }
+                if(watergirl.contains(bGem.getX(), bGem.getY())){
+                    pane.getChildren().remove(bGem);
+                    wGem += 1;
+                }
+                if(watergirl.contains(bGem1.getX(), bGem1.getY())){
+                    pane.getChildren().remove(bGem1);
+                    wGem += 1;
+                }
                 //X and Y constraints
                 if (fireboy.getX() < 0) {
                     fireboy.setX(0);
@@ -147,10 +190,10 @@ public class FWPaneFinal extends Application {
                     watergirl.setX(0);
                 }
 
-                if (fireboy.getX() >= pane.getWidth() - 85||watergirl.getX()>pane.getWidth() - 85) {
+                if ((fireboy.getX() >= pane.getWidth() - 85||watergirl.getX()>=pane.getWidth() - 85)) {
                     Rectangle rectangle = new Rectangle(pane.getWidth(), pane.getHeight());
                     rectangle.setFill(Color.BLACK);
-                    Text text = new Text(450, 325, "Congrats! You Win!");
+                    Text text = new Text(450, 325, "Congrats! You Win!\nFireboy: %d Points\nWatergirl:"+wGem+ " Points");
                     text.setFont(Font.font("Times New Roman", 36));
                     text.setFill(Color.WHITE);
                     pane.getChildren().add(rectangle);
@@ -159,42 +202,43 @@ public class FWPaneFinal extends Application {
                     ns.stop();
                     moveL.stop();
                 }
-                if (fireboy.getX() >= 470 && fireboy.getX() <= 480 && fireboy.getY()>370) {
-                    moveR.stop();
-                    fireboy.setX(470);
-                }
-                if (fireboy.getX() <= 730 && fireboy.getX() >= 720 && fireboy.getY()>370) {
-                    moveL.stop();
-                    fireboy.setX(730);
-                }
-                if (watergirl.getX() >= 470 && watergirl.getX() <= 480 && watergirl.getY()>370) {
+                if (watergirl.getX() >= stair.getX()-85 && watergirl.getX() <= stair.getX()+10 && watergirl.getY()>stair.getY()-80) {
                     moveR1.stop();
-                    watergirl.setX(470);
+                    watergirl.setX(stair.getX()-80);
                 }
-                if (watergirl.getX() <= 730 && watergirl.getX() >= 720 && watergirl.getY()>370) {
+                if (watergirl.getX() <= stair.getX()+stair.getWidth() && watergirl.getX() >= stair.getX()+stair.getWidth()-10 && watergirl.getY()>stair.getY()-80) {
                     moveL2.stop();
-                    watergirl.setX(730);
+                    watergirl.setX(stair.getX()+stair.getWidth());
                 }
-                if (stair.contains(fireboy.getX()+50, fireboy.getY() + 83)) {
-                    if (fireboy.getY() <= 400) {
-                        fireboy.setY(350);
-                        ns.pause();
-                    }
-                }
-                else{
-                    if (fireboy.getY() <= 400) {
-                        ns.play();
-                    }
-                }
-                if(stair.contains(watergirl.getX()+50, watergirl.getY()+83)){
-                    if(watergirl.getY()<=400){
-                        watergirl.setY(350);
+                if (stair.contains(watergirl.getX()+50, watergirl.getY() + 83)) {
+                    if (watergirl.getY() <= stair.getY()-10) {
+                        watergirl.setY(stair.getY()-80);
                         ns1.pause();
                     }
                 }
                 else{
-                    if (watergirl.getY() <= 400) {
+                    if (watergirl.getY() <= stair.getY()-10) {
                         ns1.play();
+                    }
+                }
+                //USE THIS PLEASE
+                if (fireboy.getX() >= stair.getX()-85 && fireboy.getX() <= stair.getX()+10 && fireboy.getY()>stair.getY()-80) {
+                    moveR.stop();
+                    fireboy.setX(stair.getX()-80);
+                }
+                if (fireboy.getX() <= stair.getX()+stair.getWidth() && fireboy.getX() >= stair.getX()+stair.getWidth()-10 && fireboy.getY()>stair.getY()-80) {
+                    moveL.stop();
+                    fireboy.setX(stair.getX()+stair.getWidth());
+                }
+                if (stair.contains(fireboy.getX()+50, fireboy.getY() + 83)) {
+                    if (fireboy.getY() <= stair.getY()-10) {
+                        fireboy.setY(stair.getY()-80);
+                        ns.pause();
+                    }
+                }
+                else{
+                    if (fireboy.getY() <= stair.getY()-10) {
+                        ns.play();
                     }
                 }
 
@@ -203,9 +247,9 @@ public class FWPaneFinal extends Application {
 
         pane.getChildren().addAll(stair, fireboy, watergirl, new FWPaneFinal.hedgePane(0,groundY),new FWPaneFinal.hedgePane(150,groundY),new FWPaneFinal.hedgePane(300,groundY),
                 new FWPaneFinal.hedgePane(450,groundY),new FWPaneFinal.hedgePane(600,groundY), new FWPaneFinal.hedgePane(750,groundY),
-                new FWPaneFinal.hedgePane(900,groundY),new FWPaneFinal.hedgePane(1050,groundY),fire, water);
+                new FWPaneFinal.hedgePane(900,groundY),new FWPaneFinal.hedgePane(1050,groundY),fire, water, bGem, bGem1,rGem, rGem1);
 
-        Scene scene = new Scene(pane, 1200,650);
+        Scene scene = new Scene(pane, 1200,950);
         firstStage.setScene(scene);
         firstStage.show();
         pane.requestFocus();
